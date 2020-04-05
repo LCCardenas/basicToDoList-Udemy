fc7cc4fb7a829a098529622341b19ceb9b059658
lccardenas <lccvlccv@mailbox.org>
2020-04-05 10:19:05 +0200

    date module refactored. Footer/header templates added

--- app.js
+++ app.js
@@ -1,26 +1,40 @@
 const express = require("express");
 const app = express();
-const https = require("https");
+const date = require(__dirname + "/date.js");
 app.use(express.urlencoded({ extended: true })); // for parsing the HTTP request
 app.use(express.static("public"));
 app.set("view engine", "ejs");
-var taskList = ["Buy food"];
+const taskList = ["Buy food"];
+const workList = [];
 
 app.get("/", function (req, res) {
-  var today = new Date();
-  var options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  };
-  var day = today.toLocaleDateString("en-US", options);
-  res.render("list", { kindOfDay: day, newListItems: taskList }); // render for EJS, send for HTML
+  let day = date.getDate();
+  res.render("list", { listTitle: day, newListItems: taskList }); // render for EJS, send for HTML
+});
+
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work", newListItems: workList }); // render for EJS, send for HTML
+});
+
+app.get("/about", function (req, res) {
+  res.render("about.ejs");
 });
 
 app.post("/", function (req, res) {
   var newTask = req.body.newTask;
-  taskList.push(newTask);
-  res.redirect("/");
+  if (req.body.list === "Work") {
+    workList.push(newTask);
+    res.redirect("/work");
+  } else {
+    taskList.push(newTask);
+    res.redirect("/");
+  }
+});
+
+app.post("/work", function (req, res) {
+  var newTask = req.body.newTask;
+  workList.push(newTask);
+  res.redirect("/work");
 });
 
 app.listen(3000, function () {
